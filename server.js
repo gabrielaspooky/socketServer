e4f3aec122d6a8c6bf72f097a8bf95ebd0054999
gabrielaspooky <gabrielanava01@gmail.com>
2024-09-10 13:31:48 +0200

    init

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado:', socket.id);
+
+  socket.on('mensaje_cliente', (msg) => {
+    console.log(`Mensaje recibido: ${msg}`);
+    io.emit('mensaje_servidor', `Servidor recibió: ${msg}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor Socket.IO corriendo en puerto ${PORT}`);
+});
