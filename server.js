7e02ffa3ce062f1cc1856a2c580852eb3c266ba5
gabrielaspooky <gabrielanava01@gmail.com>
2024-09-26 00:06:05 +0200

    server rollback

--- server.js
+++ server.js
@@ -19,26 +19,26 @@ let timerData = {
 };
 
 let users = {}; // Para llevar el registro de los usuarios en la sala
+let nextUserId = 1; // Contador para asignar IDs secuenciales
 
 const emitTimerData = (room) => {
   io.to(room).emit('timer_update', timerData);
-};
-
-const updateUsers = (room) => {
-  io.to(room).emit('update_users', users);
 };
 
 io.on('connection', (socket) => {
   console.log('Nuevo cliente conectado:', socket.id);
 
   // Unirse a una sala
-  socket.on('join_room', ({ room, username }) => {
+  socket.on('join_room', (room) => {
     socket.join(room);
-    console.log(`Cliente ${socket.id} (${username}) se unió a la sala ${room}`);
+    console.log(`Cliente ${socket.id} se unió a la sala ${room}`);
 
+    // Asignar ID secuencial
+    const userId = nextUserId++;
+    
     // Registrar usuario
-    users[socket.id] = { id: socket.id, username, activity: 'esperando' };
-    updateUsers(room);
+    users[socket.id] = { id: userId, name: `User ${userId}` };
+    io.to(room).emit('user_joined', users);
 
     socket.emit('timer_update', timerData);
   });
@@ -47,35 +47,34 @@ io.on('connection', (socket) => {
   socket.on('update_timer', (data) => {
     timerData = { ...timerData, ...data };
     const rooms = Object.keys(socket.rooms).filter(room => room !== socket.id);
-    rooms.forEach(room => {
-      emitTimerData(room);
-      // Actualizar la actividad del usuario
-      if (users[socket.id]) {
-        users[socket.id].activity = data.isActive ? (data.isBreak ? 'descanso' : 'pomodoro') : 'esperando';
-        updateUsers(room);
-      }
-    });
+    rooms.forEach(room => emitTimerData(room));
   });
 
   // Abandonar sala de manera explícita
-  socket.on('leave_room', ({ room, username }) => {
+  socket.on('leave_room', (room) => {
     socket.leave(room);
-    console.log(`Cliente ${socket.id} (${username}) abandonó la sala ${room}`);
+    console.log(`Cliente ${socket.id} abandonó la sala ${room}`);
+
+    // Obtener datos del usuario que abandona antes de eliminarlo
+    const userWhoLeft = users[socket.id];
 
     // Eliminar usuario de la sala
     delete users[socket.id];
-    updateUsers(room);
+    io.to(room).emit('user_left', userWhoLeft); // Notificar a los demás usuarios
   });
 
   // Manejar desconexiones
   socket.on('disconnect', () => {
     console.log('Cliente desconectado:', socket.id);
 
+    // Obtener datos del usuario que se desconectó antes de eliminarlo
+    const userWhoLeft = users[socket.id];
+
     // Eliminar usuario de todas las salas a las que estaba unido y notificar
     for (const room of Object.keys(socket.rooms)) {
-      if (room !== socket.id) {
+      if (room !== socket.id) { // Evitar que elimine del propio socket id (que no es una sala)
         delete users[socket.id];
-        updateUsers(room);
+        io.to(room).emit('user_left', userWhoLeft);
       }
     }
   });
